91f7a52f6d27fa8beb03d3a65af459709291c0b6
rohanj13 <rohan.australia@gmail.com>
2025-04-15 22:26:22 +1000

    added nullable fields to JobDetail

--- RealtyReachApi/Models/JobDetail.cs
+++ RealtyReachApi/Models/JobDetail.cs
@@ -16,9 +16,9 @@ public class JobDetail
     public ICollection<JobProfessionalLink> JobProfessionalLinks { get; set; } = new List<JobProfessionalLink>();
     public ICollection<ProfessionalType> ProfessionalTypes { get; set; } = new List<ProfessionalType>();
     //public List<int>  SuburbIds { get; set; } = new List<int>();
-    public List<string> Regions { get; set; }
-    public List<AustralianState> States { get; set; }
-    public List<Specialisation> Specialisations { get; set; } = new List<Specialisation>();
+    public List<string>? Regions { get; set; }
+    public List<AustralianState>? States { get; set; }
+    public List<Specialisation>? Specialisations { get; set; } = new List<Specialisation>();
     [Required] public required string PurchaseType { get; set; } // First Home/Investment Property
 
     [Required] public required string PropertyType { get; set; } // Comma Separated Strings of Property Types
