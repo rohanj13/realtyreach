c49b6e7b25468ea54466e9b3a5cc9fb84cb7a1b6
rohanj13 <rohan.australia@gmail.com>
2025-10-30 23:49:57 +1100

    migration files added

--- RealtyReachApi/Migrations/20251024051711_RequiredSuggestedProfs.Designer.cs
+++ RealtyReachApi/Migrations/20251024051711_RequiredSuggestedProfs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RealtyReachApi.Data;
@@ -12,9 +13,11 @@ using RealtyReachApi.Data;
 namespace RealtyReachApi.Migrations
 {
     [DbContext(typeof(SharedDbContext))]
-    partial class SharedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251024051711_RequiredSuggestedProfs")]
+    partial class RequiredSuggestedProfs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
